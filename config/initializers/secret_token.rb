552aa31de4d5c5a00a6eca797549653af5d33db8
Aleksej Leonov <mail@aleksejleonov.com>
2011-08-09 23:53:49 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aacimp::Application.config.secret_token = 'df2a728486051bb853a67ab49721560fab8f61acd5c50ba2a6064eeda9f2e037d8185b667198587e75ac06f582d8a28faa6b4084aced72785cdc8f3ed8c4326b'
